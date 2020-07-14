d8c949644781b943494b4f89fc9f867d46888ffd
Myat Thu Ko <nymyatthuko@gmail.com>
2020-07-14 15:39:01 -0400

    Initial Commit

---
+++ Friends Face/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friends Face
+//
+//  Created by Myat Thu Ko on 7/14/20.
+//  Copyright © 2020 Myat Thu Ko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
